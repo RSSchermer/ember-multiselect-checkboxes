fc18d94ccb38ebaf067ce6802c057508aa0b727f
R.S. Schermer <roland0507@gmail.com>
2015-03-22 18:42:53 +0100

    Updaged to ember-cli 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Ember-multiselect-checkboxes Changelog
 
+## 0.2.0
+
+Upgraded to Ember CLI 0.2.0.
+
 ## 0.1.0
 
 Added option to disable a checkbox group, courtesy of @rafaelsales (see #6):
--- tests/unit/components/multiselect-checkbox-option-test.js
+++ tests/unit/components/multiselect-checkbox-option-test.js
@@ -5,22 +5,22 @@ import Ember from 'ember';
 var App;
 
 moduleForComponent('multiselect-checkbox-option', 'MultiselectCheckboxOptionComponent', {
-  setup: function() {
+  beforeEach: function() {
     App = startApp();
   },
-  teardown: function() {
+  afterEach: function() {
     Ember.run(App, 'destroy');
   }
 });
 
-test('user the correct label property with a primitive value and no label property', function () {
+test('user the correct label property with a primitive value and no label property', function (assert) {
   var component = this.subject();
 
   Ember.run(function(){
     component.setProperties({ 'value': 'apple' });
   });
 
-  equal(this.$().find('label').text().trim(), 'apple');
+  assert.equal(this.$().find('label').text().trim(), 'apple');
 });
 
 var car = {
@@ -28,14 +28,14 @@ var car = {
   color: 'red'
 };
 
-test('user the correct label property with a plain js object', function () {
+test('user the correct label property with a plain js object', function (assert) {
   var component = this.subject();
 
   Ember.run(function(){
     component.setProperties({ 'value': car, 'labelProperty': 'color' });
   });
 
-  equal(this.$().find('label').text().trim(), 'red');
+  assert.equal(this.$().find('label').text().trim(), 'red');
 });
 
 var person = Ember.Object.create({
@@ -43,69 +43,69 @@ var person = Ember.Object.create({
     name: 'test'
   });
 
-test('uses the correct label property with an ember object', function() {
+test('uses the correct label property with an ember object', function(assert) {
   var component = this.subject();
 
   Ember.run(function(){
     component.setProperties({ 'value': person, 'labelProperty': 'name' });
   });
 
-  equal(this.$().find('label').text().trim(), 'test');
+  assert.equal(this.$().find('label').text().trim(), 'test');
 });
 
-test('adds value to selection when selected', function() {
+test('adds value to selection when selected', function(assert) {
   var component = this.subject();
 
   Ember.run(function(){
     component.setProperties({ 'value': person, 'labelProperty': 'name' });
   });
 
-  equal(component.get('isSelected'), false);
-  equal(component.get('selection.length'), 0);
+  assert.equal(component.get('isSelected'), false);
+  assert.equal(component.get('selection.length'), 0);
 
   Ember.run(function(){
     component.set('isSelected', true);
   });
 
-  equal(component.get('selection.length'), 1);
-  equal(component.get('selection.firstObject'), person);
+  assert.equal(component.get('selection.length'), 1);
+  assert.equal(component.get('selection.firstObject'), person);
 });
 
-test('removes value from selection when deselected', function() {
+test('removes value from selection when deselected', function(assert) {
   var component = this.subject();
 
   Ember.run(function(){
     component.setProperties({ 'value': person, 'labelProperty': 'name', 'selection': [person] });
   });
 
-  equal(component.get('isSelected'), true);
-  equal(component.get('selection.length'), 1);
+  assert.equal(component.get('isSelected'), true);
+  assert.equal(component.get('selection.length'), 1);
 
   Ember.run(function(){
     component.set('isSelected', false);
   });
 
-  equal(component.get('selection.length'), 0);
+  assert.equal(component.get('selection.length'), 0);
 });
 
-test('correctly updates isSelected when changing the selection', function() {
+test('correctly updates isSelected when changing the selection', function(assert) {
   var component = this.subject();
 
   Ember.run(function(){
     component.setProperties({ 'value': person, 'labelProperty': 'name', 'selection': [] });
   });
 
-  equal(component.get('isSelected'), false);
+  assert.equal(component.get('isSelected'), false);
 
   Ember.run(function(){
     component.set('selection', [person]);
   });
 
-  equal(component.get('isSelected'), true);
+  assert.equal(component.get('isSelected'), true);
 
   Ember.run(function(){
     component.set('selection', []);
   });
 
-  equal(component.get('isSelected'), false);
+  assert.equal(component.get('isSelected'), false);
 });
