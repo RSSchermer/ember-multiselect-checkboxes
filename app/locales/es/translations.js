8a2d38855f5b639659fd78cd4ca58231577f28c0
Megan Slater <mslater@MSLATER-MAC.local>
2016-02-02 15:23:21 -0800

    Remove extra comments

--- app/locales/es/translations.js
+++ app/locales/es/translations.js
@@ -2,13 +2,4 @@ export default {
   "Lisa": "Luisa",
   "Bob": "Roberto",
   "John": "Juan"
-  // "some.translation.key": "Text for some.translation.key",
-  //
-  // "a": {
-  //   "nested": {
-  //     "key": "Text for a.nested.key"
-  //   }
-  // },
-  //
-  // "key.with.interpolation": "Text with {{anInterpolation}}"
 };
