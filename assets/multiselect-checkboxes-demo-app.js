d614b0101ed3f9196a99e275737456da68f3bedc
R.S. Schermer <roland0507@gmail.com>
2017-03-20 22:59:53 +0100

    Try without baseURL or rootURL entirely

--- assets/multiselect-checkboxes-demo-app.js
+++ assets/multiselect-checkboxes-demo-app.js
@@ -313,6 +313,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("multiselect-checkboxes-demo-app/app")["default"].create({"name":"multiselect-checkboxes-demo-app","version":"0.0.0+2ec78f78"});
+  require("multiselect-checkboxes-demo-app/app")["default"].create({"name":"multiselect-checkboxes-demo-app","version":"0.0.0+6723d754"});
 }
 //# sourceMappingURL=multiselect-checkboxes-demo-app.map
